580f54b885ad157780a7174d3d25ee97f788bb82
Eduardo Sousa <eduardosousa@eduardos-air.home>
2024-11-11 20:14:07 -0500

    feat: add filter to health recods screen

--- src/screens/HealthRecordsScreen/index.tsx
+++ src/screens/HealthRecordsScreen/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { FlatList, Alert, Modal } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import { Ionicons } from '@expo/vector-icons';
 import {
   Container,
   ListItem,
@@ -19,21 +19,36 @@ import {
   EmptyListImage,
   EmptyListText,
   DisabledAddButton,
+  CloseButton,
+  ModalHeader,
+  DropdownContainer,
+  FloatingFilterButton,
+  DropdownLabel,
+  DropdownStyle,
+  FloatingClearFilterButton,
 } from './styles';
 import { DogProfileContext } from '../../context/DogProfileContext';
 import { db } from '../../firebase/Firestore';
 import * as Icon from 'phosphor-react-native';
-import healthRecordsImage from '../../assets/healthRecords.png'
+import healthRecordsImage from '../../assets/healthRecords.png';
 import { useFocusEffect } from '@react-navigation/native';
+import { Dropdown } from 'react-native-element-dropdown';
+import { X } from 'phosphor-react-native';
 
 export default function HealthRecordsScreen({ navigation }) {
   const [healthRecords, setHealthRecords] = useState([]);
   const [filteredRecords, setFilteredRecords] = useState([]);
-  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const [showDateModal, setShowDateModal] = useState(false); // Modal state
-  const [isFilterApplied, setIsFilterApplied] = useState(false); // Filter state
+  const [selectedType, setSelectedType] = useState(''); // New state for type filter
+  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [selectedYear, setSelectedYear] = useState(null);
+  const [showDateModal, setShowDateModal] = useState(false);
+  const [isFilterApplied, setIsFilterApplied] = useState(false);
+
   const { selectedDog } = useContext(DogProfileContext);
+
+  const hasActiveFilters = () => {
+    return selectedType || selectedMonth !== null || selectedYear !== null;
+  };
 
   useFocusEffect(
     React.useCallback(() => {
@@ -50,7 +65,7 @@ export default function HealthRecordsScreen({ navigation }) {
             .where('dogId', '==', selectedDog.id)
             .get();
 
-          const records = recordsSnapshot.docs.map(doc => ({
+          const records = recordsSnapshot.docs.map((doc) => ({
             id: doc.id,
             ...doc.data(),
           }));
@@ -71,22 +86,60 @@ export default function HealthRecordsScreen({ navigation }) {
     }, [selectedDog])
   );
 
+const currentMonth = new Date().getMonth(); // Get the current month (0-based, 0 = January, 11 = December)
+
+
+const months = Array.from({ length: 12 }, (_, index) => ({
+  label:
+    index === currentMonth
+      ? `${new Date(2023, index).toLocaleString('default', {
+          month: 'long',
+        })} (Current Month)`
+      : new Date(2023, index).toLocaleString('default', { month: 'long' }),
+  value: index,
+}));
+
+ // Filter function
+const filterRecords = () => {
+  const filtered = healthRecords.filter((record) => {
+    const recordDate = new Date(record.date); // Date object of the record
+
+    // Check each filter and only apply if there’s a selected value
+    const typeMatches = selectedType ? record.type === selectedType : true;
+    const monthMatches =
+      selectedMonth !== null ? recordDate.getMonth() === selectedMonth : true;
+    const yearMatches =
+      selectedYear !== null ? recordDate.getFullYear() === selectedYear : true;
+
+    return typeMatches && monthMatches && yearMatches;
+  });
+
+  filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  setFilteredRecords(filtered);
+  setIsFilterApplied(true);
+  setShowDateModal(false);
+
+  // Reset filters after applying, if desired
+  clearFilters();
+};
+
   const addHealthRecord = (newRecord) => {
     setHealthRecords([...healthRecords, newRecord]);
   };
 
   const confirmDelete = (id) => {
     Alert.alert(
-      "Delete Health Record",
-      "Are you sure you want to delete this health record?",
+      'Delete Health Record',
+      'Are you sure you want to delete this health record?',
       [
         {
-          text: "Cancel",
-          style: "cancel",
+          text: 'Cancel',
+          style: 'cancel',
         },
         {
-          text: "Delete",
-          style: "destructive",
+          text: 'Delete',
+          style: 'destructive',
           onPress: () => deleteHealthRecord(id),
         },
       ],
@@ -97,34 +150,14 @@ export default function HealthRecordsScreen({ navigation }) {
   const deleteHealthRecord = async (id) => {
     try {
       await db.collection('healthRecords').doc(id).delete();
-      setHealthRecords((prevRecords) => prevRecords.filter((record) => record.id !== id));
+      setHealthRecords((prevRecords) =>
+        prevRecords.filter((record) => record.id !== id)
+      );
       Alert.alert('Success', 'Health record deleted successfully');
     } catch (error) {
       console.error('Error deleting health record', error);
       Alert.alert('Error', 'Failed to delete health record');
     }
-  };
-
-  const filterRecords = () => {
-    const filtered = healthRecords.filter(record => {
-      const recordDate = new Date(record.date);
-
-      // Compare record date with selected month and year
-      return recordDate.getMonth() === selectedMonth && recordDate.getFullYear() === selectedYear;
-    });
-
-    // Sort filtered records by date in descending order
-    filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-    setFilteredRecords(filtered);
-
-    setIsFilterApplied(true); // Activate filter
-    setShowDateModal(false); // Close modal after filter
-  };
-
-  const resetFilter = () => {
-    setIsFilterApplied(false); // Deactivate filter
-    setFilteredRecords([]); // Clear filtered records
   };
 
   const getTypeIcon = (type) => {
@@ -149,18 +182,24 @@ export default function HealthRecordsScreen({ navigation }) {
       day: 'numeric',
     });
 
-     // Determine what text to show based on the type
-  const displayText = item.type === 'Medication' || item.type === 'Vaccine' ? item.extraInfo : item.type;
-
+    // Determine what text to show based on the type
+    const displayText =
+      item.type === 'Medication' || item.type === 'Vaccine'
+        ? item.extraInfo
+        : item.type;
 
     return (
-      
-      <ListItem onPress={() => navigation.navigate('HealthRecordDetails', { record: item })}>
+      <ListItem
+        onPress={() =>
+          navigation.navigate('HealthRecordDetails', { record: item })
+        }
+      >
         <TypeIcon>{getTypeIcon(item.type)}</TypeIcon>
         <ListItemContent>
-        <ListItemText>{displayText || item.type}</ListItemText>
+          <ListItemText>{displayText || item.type}</ListItemText>
           <ListItemDetailHint>
-            Tap to view or edit details <DetailDateText>• {formattedDate}</DetailDateText>
+            Tap to view or edit details{' '}
+            <DetailDateText>• {formattedDate}</DetailDateText>
           </ListItemDetailHint>
         </ListItemContent>
         <TrashIconContainer onPress={() => confirmDelete(item.id)}>
@@ -170,75 +209,160 @@ export default function HealthRecordsScreen({ navigation }) {
     );
   };
 
+  // Clear filter function to reset all selected filter values
+  const clearFilters = () => {
+    setSelectedType(null);
+    setSelectedMonth(null);
+    setSelectedYear(null);
+  };
+
+  // Modify renderFilterModal to reset filters on close
+  const renderFilterModal = () => (
+    <Modal
+      visible={showDateModal}
+      animationType="fade"
+      transparent={true}
+      onRequestClose={() => {
+        clearFilters(); // Reset filters when modal closes
+        setShowDateModal(false);
+      }}
+    >
+      <Container
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        }}
+      >
+        <ModalContainer>
+          <ModalHeader>
+            <ModalTitle>Filter Health Records</ModalTitle>
+            <CloseButton
+              onPress={() => {
+                clearFilters(); // Reset filters on pressing close
+                setShowDateModal(false);
+              }}
+            >
+              <X size={32} />
+            </CloseButton>
+          </ModalHeader>
+
+          {/* Type Filter */}
+          <DropdownContainer>
+            <DropdownLabel>Filter by Type</DropdownLabel>
+            <DropdownStyle>
+              <Dropdown
+                data={[
+                  { label: 'All Types', value: null },
+                  { label: 'Vaccine', value: 'Vaccine' },
+                  { label: 'Vet Appointment', value: 'Vet Appointment' },
+                  { label: 'Medication', value: 'Medication' },
+                  { label: 'Pet Groomer', value: 'Pet Groomer' },
+                ]}
+                labelField="label"
+                valueField="value"
+                placeholder="Select Type"
+                value={selectedType}
+                onChange={(item) => setSelectedType(item.value)}
+              />
+            </DropdownStyle>
+          </DropdownContainer>
+
+          {/* Month Filter */}
+          <DropdownContainer>
+            <DropdownLabel>Filter by Month</DropdownLabel>
+            <DropdownStyle>
+              <Dropdown
+                data={months}
+                labelField="label"
+                valueField="value"
+                placeholder="Select Month"
+                value={selectedMonth}
+                onChange={(item) => setSelectedMonth(item.value)}
+              />
+            </DropdownStyle>
+          </DropdownContainer>
+
+          {/* Year Filter */}
+          <DropdownContainer>
+            <DropdownLabel>Filter by Year</DropdownLabel>
+            <DropdownStyle>
+              <Dropdown
+                data={Array.from({ length: 10 }, (_, index) => {
+                  const year = new Date().getFullYear() - index;
+                  return { label: `${year}`, value: year };
+                })}
+                labelField="label"
+                valueField="value"
+                placeholder="Select Year"
+                value={selectedYear}
+                onChange={(item) => setSelectedYear(item.value)}
+              />
+            </DropdownStyle>
+          </DropdownContainer>
+
+          {/* Apply Filter Button */}
+          <AddButton onPress={filterRecords} disabled={!hasActiveFilters()}>
+            <ButtonText style={{ opacity: hasActiveFilters() ? 1 : 0.5 }}>
+              Apply Filter
+            </ButtonText>
+          </AddButton>
+        </ModalContainer>
+      </Container>
+    </Modal>
+  );
+
   const renderEmptyList = () => (
     <EmptyListContainer>
       <EmptyListImage source={healthRecordsImage} />
       <EmptyListText>
-        No health records yet. Add your first pet and start adding records to keep track of your pet’s health.
+        No health records yet. Add your first pet and start adding records to
+        keep track of your pet’s health.
       </EmptyListText>
     </EmptyListContainer>
   );
 
   return (
     <Container>
-      
-      {/* <FilterButton onPress={() => setShowDateModal(true)}>
-        <ButtonText>Filter by Month & Year</ButtonText>
-      </FilterButton> */}
-      
       {isFilterApplied && (
-        <FilterButton onPress={resetFilter}>
-          <ButtonText>Clear Filter</ButtonText>
-        </FilterButton>
+        <FloatingClearFilterButton
+          onPress={() => {
+            clearFilters();
+            setIsFilterApplied(false);
+            setFilteredRecords([]);
+          }}
+        >
+          <Ionicons name="close" size={20} color="#FFF" />
+        </FloatingClearFilterButton>
       )}
 
-      <Modal
-        visible={showDateModal}
-        animationType="slide"
-        onRequestClose={() => setShowDateModal(false)}
-      >
-        <ModalContainer>
-          <ModalTitle>Select Month and Year</ModalTitle>
-          
-          <Picker
-            selectedValue={selectedMonth}
-            onValueChange={(itemValue) => setSelectedMonth(itemValue)}
-            style={{ width: '100%', marginBottom: 10 }}
-          >
-            {Array.from({ length: 12 }, (_, index) => (
-              <Picker.Item  key={`month-${index}-${new Date().getTime()}`} label={new Date(0, index).toLocaleString('default', { month: 'long' })} value={index} />
-            ))}
-          </Picker>
-          
-          <Picker
-            selectedValue={selectedYear}
-            onValueChange={(itemValue) => setSelectedYear(itemValue)}
-            style={{ width: '100%' }}
-          >
-            {Array.from({ length: 10 }, (_, index) => {
-              const year = new Date().getFullYear() - index;
-              return <Picker.Item key={`year-${year}-${index}-${new Date().getTime()}`} label={`${year}`} value={year} />;
-            })}
-          </Picker>
-          
-          <AddButton onPress={filterRecords}>
-            <ButtonText>Apply Filter</ButtonText>
-          </AddButton>
-        </ModalContainer>
-      </Modal>
+      {renderFilterModal()}
 
       <FlatList
         data={isFilterApplied ? filteredRecords : healthRecords}
         renderItem={renderRecord}
         keyExtractor={(item, index) => `${item.id}-${index}`}
         showsVerticalScrollIndicator={false}
-        ListEmptyComponent={renderEmptyList} // Display image if list is empty
+        ListEmptyComponent={renderEmptyList}
+        contentContainerStyle={{ paddingBottom: 80 }}
       />
-      
+
       {selectedDog ? (
-        <AddButton onPress={() => navigation.navigate('AddHealthRecord', { addRecord: addHealthRecord })}>
-          <ButtonText>Add Health Record</ButtonText>
-        </AddButton>
+        <>
+          <AddButton
+            onPress={() =>
+              navigation.navigate('AddHealthRecord', {
+                addRecord: addHealthRecord,
+              })
+            }
+          >
+            <ButtonText>Add Health Record</ButtonText>
+          </AddButton>
+          <FloatingFilterButton onPress={() => setShowDateModal(true)}>
+            <Ionicons name="filter" size={20} color="#FFF" />
+          </FloatingFilterButton>
+        </>
       ) : (
         <DisabledAddButton disabled>
           <ButtonText>Add Health Record</ButtonText>
